cad4ec6db4cfa09512a4c199aff45122d9d9c0cc
Matias Cardone <matias.cardone@outlook.com>
2021-02-24 15:28:09 -0300

    Changing font-family with typography

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typograpy from 'typography';
+
+const typography = new Typograpy({
+    baseFontSize: "18px",
+    baseLineHeight: 1.666,
+    headerFontFamily: ["Lato", "Helvetica Neue", "Arial"],
+    bodyFontFamily: ["Open Sans", "Roboto", "Georgia"],
+})
+
+export default typography;
